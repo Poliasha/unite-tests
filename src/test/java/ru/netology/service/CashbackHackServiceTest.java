cd5a3e2c83fec8ad7dbaa78449afc7bdc2a99226
Poliasha <poli11@yandex.ru>
2020-01-23 23:09:07 +0300

    initial commit

---
+++ src/test/java/ru/netology/service/CashbackHackServiceTest.java
@@ -0,0 +1,36 @@
+package ru.netology.service;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class CashbackHackServiceTest {
+
+    @Test
+    void shouldReturn100IfAmount900() {
+        CashbackHackService cashbackHackService = new CashbackHackService();
+        int amount = 900;
+        int actual = cashbackHackService.remain(amount);
+        int expected = 100;
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    void shouldReturnZeroIfAmountIs1000(){
+        CashbackHackService cashbackHackService = new CashbackHackService();
+        int amount = 1000;
+        int actual = cashbackHackService.remain(amount);
+        int expected = 0;
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    void shouldReturnValidCashBack(){
+        CashbackHackService cashbackHackService = new CashbackHackService();
+        int amount = 1001;
+        int actual = cashbackHackService.remain(amount);
+        int expected = 999;
+        assertEquals(expected, actual);
+    }
+
+}
